989d45d9212a041d3635cf109d90aad96a9833e0
Alex Bermejo <alex@MacBook-Pro-de-Alex.local>
2025-05-19 12:31:39 +0200

    image upload and show.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import {useRef, useState} from "react";
 
 function App() {
+  const [image, setImage] = useState(null);
+  const canvasRef = useRef()
+
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    if (!file) return; // don't do anything
+
+    const reader = new FileReader();
+    // when file is read
+    reader.onload = (event) => {
+      const img = new  Image();
+      img.onload = () => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d'); //renderer
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0); // show image
+        setImage(img); // load variable
+      };
+      img.src = event.target.result; //set image
+    };
+    reader.readAsDataURL(file);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{padding: '20px'}}>
+        <h1>Upload Image</h1>
+        <input type="file" accept="image/*" onChange={handleImageUpload} />
+        <div style={{ marginTop: '20px' }}>
+          <canvas ref={canvasRef} style={{ border: '1px solid black', maxWidth: '80%' }} />
+        </div>
+      </div>
   );
 }
 
